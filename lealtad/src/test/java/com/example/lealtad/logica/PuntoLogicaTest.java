71b0cf0c00dd509ca400725ce833833d56685b09
Samuelrg123 <71299069+Samuelrg123@users.noreply.github.com>
2023-09-26 23:40:33 -0500

    Agrega nuevas pruebas unitarias
    Soluciona error en prueba de TransaccionLogicaTest anterior. Añade pruebas unitarias de PuntoLogica e HistorialLogica

---
+++ lealtad/src/test/java/com/example/lealtad/logica/PuntoLogicaTest.java
@@ -0,0 +1,42 @@
+package com.example.lealtad.logica;
+
+import com.example.lealtad.bd.repository.PuntoRepository;
+import com.example.lealtad.controller.dto.PuntoDTO;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PuntoLogicaTest {
+    @Mock
+    private PuntoRepository puntoRepository;
+    @InjectMocks
+    PuntoLogica puntoLogica;
+
+    @Test
+    void Dado_cliente_con_puntos_2_Cuando_findPuntosAcumuladosByCliente_Entonces_puntosActuales_2() {
+        PuntoDTO puntoDTO = new PuntoDTO(1);
+        double puntos = 2.0;
+
+        when(puntoRepository.findPuntosAcumuladosByCliente(1)).thenReturn(puntos);
+        double puntosActuales = puntoLogica.obtenerPuntosActuales(puntoDTO);
+
+        assertEquals(2, puntosActuales);
+    }
+
+    @Test
+    void Dado_cliente_con_puntosAcumulados_2_Cuando_actualizarPuntos_Entonces_updatePuntossetPuntosAcumulados() {
+        PuntoDTO puntoDTO = new PuntoDTO(1);
+        puntoDTO.setPuntosAcumulados(2);
+
+        doNothing().when(puntoRepository).updatePuntosetPuntosAcumulados(anyInt(), anyDouble());
+        puntoLogica.actualizarPuntos(puntoDTO);
+        Mockito.verify(puntoRepository).updatePuntosetPuntosAcumulados(puntoDTO.getCliente(), puntoDTO.getPuntosAcumulados());
+    }
+}
