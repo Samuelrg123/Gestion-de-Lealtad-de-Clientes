e57130087e7c16ff78fbefb5e12e0b71fb833c05
Samuelrg123 <71299069+Samuelrg123@users.noreply.github.com>
2023-09-28 22:47:29 -0500

    agrega prueba unitaria de controlador cliente
    Agrega @NoArgsConstructor a clientedto, implementa prueba de controlador cliente y habilita permisos de gradlew

--- lealtad/src/main/java/com/example/lealtad/controller/dto/RespuestaDTO.java
+++ lealtad/src/main/java/com/example/lealtad/controller/dto/RespuestaDTO.java
@@ -2,9 +2,11 @@ package com.example.lealtad.controller.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class RespuestaDTO {
     String mensaje;
 }
