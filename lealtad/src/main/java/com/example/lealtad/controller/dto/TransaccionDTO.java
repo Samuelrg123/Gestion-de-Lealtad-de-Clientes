f875f0e4a3c6836c6e9ebbf5b4205f23e90a5980
Samuelrg123 <71299069+Samuelrg123@users.noreply.github.com>
2023-09-28 23:02:25 -0500

    Agrega permisos gradle

--- lealtad/src/main/java/com/example/lealtad/controller/dto/TransaccionDTO.java
+++ lealtad/src/main/java/com/example/lealtad/controller/dto/TransaccionDTO.java
@@ -1,7 +1,9 @@
 package com.example.lealtad.controller.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Data
 public class TransaccionDTO {
     private int cliente;
